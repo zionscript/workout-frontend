eb04f82c797c7f1ef13e864ffc6e9f6f8444e385
zionscript <wassabi1002@gmail.com>
2025-02-16 15:10:57 -0300

    setup do projeto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,20 +5,27 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+        <section className={styles.navbarSection}>
+          <nav className={styles.navbarContent}>
+            <p>WORKOUT</p>
+            <ul>
+              <li>WORKOUTS</li>
+              <li>PROGRAMS</li>
+              <li>HEALTHY LIVING</li>
+              <li>COMMUNITY</li>
+              <li>ABOUT</li>
+            </ul>
+          </nav>
+        </section>
+        <section className={styles.firstContent}>
+        <Image src="/homem.png" alt="homem amarrando o tênis" width={409} height={684}/>
+        <h1 className={styles.textHeading}>
+          WORKOUT WITH ME
+        </h1>
+        </section>
+        <h2 className={styles.textDescription}>
+        A huge selection of health and fitness content, healthy recipes and transformation stories to help you get fit and stay fit!
+        </h2>   
 
         <div className={styles.ctas}>
           <a
@@ -34,61 +41,12 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
+            Join Club Now!
           </a>
         </div>
       </main>
       <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+        <p>Mantido por &copy;Yuri Oliveira</p>
       </footer>
     </div>
   );
